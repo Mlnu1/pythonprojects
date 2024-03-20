3b0d522751ac779122631144ce71932a30490709
Bane <manjunath92ng@gmail.com>
2024-03-20 21:54:51 +0530

    basics

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # pythonprojects
 
 Day1 : to do list App creation
+Interactive program 
+ > cli
+ > desktop graphiacal user interface
+ > web interface program (web apps)
+Non-interractive Programs (back ground , cleanups)
+
+1st to do list in cli cli.py
+  it will add show complete or exit
---
+++ cli.py
@@ -0,0 +1,13 @@
+user_prompt = "Enter a todo:"
+user_text = input(user_prompt)
+print(user_text)
+## print and input are functions
+## Enter a todo: is the arg , somethin g has "" its a string
+
+#print("Enter a todo:")
+#user_test = input()
+#print(user_test)
+
+#user_text = input("Enter a todo:")
+#print(user_text)
+#
