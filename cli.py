77b83875dddcf4c20671131987e9a032cb28bb24
Bane <manjunath92ng@gmail.com>
2024-03-21 18:52:38 +0530

    update

--- cli.py
+++ cli.py
@@ -1,8 +1,14 @@
 user_prompt = "Enter a todo:"
-user_text = input(user_prompt)
-print(user_text)
+todo1 = input(user_prompt)
+todo2 = input(user_prompt)
+todo3 = input(user_prompt)
+
+todos = [todo1, todo2, todo3]
+print(todos)
+print(type(todos))
+
 ## print and input are functions
-## Enter a todo: is the arg , somethin g has "" its a string
+## Enter a todo: is the arg , something which has "" its a string
 
 #print("Enter a todo:")
 #user_test = input()
@@ -10,4 +16,13 @@ print(user_text)
 
 #user_text = input("Enter a todo:")
 #print(user_text)
-#
+
+#user_prompt = "Enter a todo:"
+#todo2 = input(user_prompt)
+#todo2 = input(user_prompt)
+#todo2 = input(user_prompt)
+#user_text = input(user_prompt)
+#print(user_text)
+
+#Day2 add multiple todo lists and fidn the type, alist is needed to store muliple objects
+#todos = [todo1, todo2, todo3, "hello"]
---
+++ test.py
@@ -0,0 +1,17 @@
+#name = input("give your name:")
+#print(name)
+
+#checklist = ("enter the four list:")
+#todo1 = input(checklist)
+#todo2 = input(checklist)
+#todo3 = input(checklist)
+#todo4 = input(checklist)
+
+#my_variable = [todo1, todo2, todo3, todo4]
+#print(my_variable)
+
+#answers = ['Yes', 'No', 'Yes', 'No', 'No'
+
+my_answer = input("What is your answer?")
+answers = ['Yes', 'No', 'Yes' 'No' "my_answer"]
+
